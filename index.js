e67cb585e5289e6e1b4594dbdc9b3514d0af933f
bentwhiskers <abbott1414@gmail.com>
2023-05-08 17:36:01 -0400

    added to require list in index.js; created writeToFile function; created init function; created generateSvg.js file

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const jest = require('jest');
+const generateSvg = require('./lib/generateSvg');
+
 
 // TODO: create an array of questions for user input
 const questions = [
@@ -28,16 +30,26 @@ const questions = [
         name: 'shapeColor',
         message: 'Enter a color name or a hexidecimal color number for your shape:'
     }
-]
+];
+
 // TODO: create a function to write an SVG file
 // must also output text "Generated logo.svg" to the command line
 // logo.svg is a 300x200 pixel img
 
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const fileName = './examples/logo.svg';
+
+    fs.writeFile(fileName, data, function (err) {
+        err ? console.log(err) : console.log('Generated ' + fileName)
+    })
+};
 
 // TODO: create a function to initialize the app
 
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then (answers => writeToFile(generateSvg(answers)))
+};
 
 // Function call to initialize app
 
