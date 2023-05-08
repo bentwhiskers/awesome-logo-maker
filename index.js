c1716887870bac6e2a76bb0d57abd3705b9c0d83
bentwhiskers <abbott1414@gmail.com>
2023-05-08 13:40:16 -0400

    created array of questions for prompt

--- index.js
+++ index.js
@@ -7,9 +7,31 @@ const jest = require('jest');
 
 // TODO: create an array of questions for user input
 const questions = [
-    {},
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for your logo text:'
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color name or a hexidecimal color number for your text:'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Pick a shape for your logo:',
+        choices: ['Circle', 'Square', 'Triangle']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color name or a hexidecimal color number for your shape:'
+    }
 ]
 // TODO: create a function to write an SVG file
+// must also output text "Generated logo.svg" to the command line
+// logo.svg is a 300x200 pixel img
 
 function writeToFile(fileName, data) {}
 
